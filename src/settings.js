578811e34c402481bd1f1439479e4f2162bf2d2a
Joe Henson <jd.henson@me.com>
2020-11-01 10:25:54 +0000

    Add .env variables to settings.js

--- src/settings.js
+++ src/settings.js
@@ -2,4 +2,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const appName = process.env.APP_NAME;
+export const lastFmUrl = process.env.LAST_FM_URL;
+export const lastFmUser = process.env.LAST_FM_USER;
+export const lastFmApiKey = process.env.LAST_FM_API_KEY;
