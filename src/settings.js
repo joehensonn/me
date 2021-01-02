e5c0f005e4807554b7b609ea103488a2dd76f756
Joe Henson <jd.henson@me.com>
2021-01-02 20:28:49 +0000

    ENV variables for new home route

--- src/settings.js
+++ src/settings.js
@@ -5,3 +5,11 @@ dotenv.config();
 export const lastFmUrl = process.env.LAST_FM_URL;
 export const lastFmUser = process.env.LAST_FM_USER;
 export const lastFmApiKey = process.env.LAST_FM_API_KEY;
+
+export const govRiverStation = process.env.GOV_RIVER_STATION;
+
+export const city = process.env.LOCATION_CITY;
+export const country = process.env.LOCATION_COUNTRY;
+
+export const weatherApiKey = process.env.OPENWEATHER_API_KEY;
+export const weatherApiCityId = process.env.OPENWEATHER_CITY_ID;
