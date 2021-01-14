4c2513352e84721f6ab7507328674c056abfaf75
Joe Henson <jd.henson@me.com>
2021-01-14 22:50:23 +0000

    Revert the revert 😩

--- src/controllers/meController.js
+++ src/controllers/meController.js
@@ -5,8 +5,14 @@
 const hello = (req, res) => {
     res.status(200).json({
         greeting: "Hi, I'm Joe 👋🏻",
-        jobTitle: "Software Developer & Freelance Digital Designer"
-    })
+        jobTitle: 'Software Developer & Freelance Digital Designer',
+        endpoints: {
+          home: '/',
+          outsideInformation: '/home/outside',
+          currentlyListening: '/music/currently-listening',
+          topSongs: '/music/top-songs',
+        }
+      });
 };
 
 export default { hello }
