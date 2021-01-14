8868378d76e835b3b3dae2b1a76062afe7af3505
Joe Henson <jd.henson@me.com>
2021-01-14 15:54:57 +0000

    Add endpoints to home route

--- src/controllers/meController.js
+++ src/controllers/meController.js
@@ -3,10 +3,16 @@
 */
 
 const hello = (req, res) => {
-    res.status(200).json({
-        greeting: "Hi, I'm Joe 👋🏻",
-        jobTitle: "Software Developer & Freelance Digital Designer"
-    })
+  res.status(200).json({
+    greeting: "Hi, I'm Joe 👋🏻",
+    jobTitle: 'Software Developer & Freelance Digital Designer',
+    endpoints: {
+      home: '/',
+      outsideInformation: '/home/outside',
+      currentlyListening: '/music/currently-listening',
+      topSongs: '/music/top-songs',
+    }
+  });
 };
 
 export default { hello }
