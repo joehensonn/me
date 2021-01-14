cb918f002e05208074f436715c80124d41de4eb6
Joe Henson <jd.henson@me.com>
2021-01-14 22:11:50 +0000

    Revert "Add endpoints to home route"
    This reverts commit 8868378d76e835b3b3dae2b1a76062afe7af3505.

--- src/controllers/meController.js
+++ src/controllers/meController.js
@@ -3,16 +3,10 @@
 */
 
 const hello = (req, res) => {
-  res.status(200).json({
-    greeting: "Hi, I'm Joe 👋🏻",
-    jobTitle: 'Software Developer & Freelance Digital Designer',
-    endpoints: {
-      home: '/',
-      outsideInformation: '/home/outside',
-      currentlyListening: '/music/currently-listening',
-      topSongs: '/music/top-songs',
-    }
-  });
+    res.status(200).json({
+        greeting: "Hi, I'm Joe 👋🏻",
+        jobTitle: "Software Developer & Freelance Digital Designer"
+    })
 };
 
 export default { hello }
