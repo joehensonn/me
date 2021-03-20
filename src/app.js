b38d0a82cd43558ec82ec8d9bc9e0f5507efa84c
Joe Henson <jd.henson@me.com>
2021-03-20 23:16:43 +0000

    Add cors and whitelist personal website to stop it failing!

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import logger from 'morgan';
 import express from 'express';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import indexRouter from './routes/index';
 
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors({origin: 'https://joehenson.co.uk', optionsSuccessStatus: 200}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
